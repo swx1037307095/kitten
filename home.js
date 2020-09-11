a29817a65f3f981be9eceaacbbf8b363eef49870
swx1037307095 <63413693+swx1037307095@users.noreply.github.com>
2020-09-11 09:14:14 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+function showButton(){
+    var b = document.getElementById("btn");
+    b.style.display = "block";
+}
+
+function hideButton(){
+    var b = document.getElementById("btn");
+    b.style.display = "none";
+}
+
+function homePage(){
+    window.location.replace("cat.html");
+}

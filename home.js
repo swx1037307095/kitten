2c145f6616b29ab4b82dc69374301c778d90d420
swx1037307095 <swx1037307095@gmail.com>
2020-09-15 09:59:31 -0400

    Add files

---
+++ home.js
@@ -0,0 +1,18 @@
+function showButton(){
+    var a = document.getElementById("btn");
+    a.style.display = "block";
+}
+
+function hideButton(){
+    var a = document.getElementById("btn");
+    a.style.display = "none";
+}
+
+function homePage(){
+    window.location.replace("cat.html");
+}
+
+function showlayer1(){
+    var a = document.getElementById("1");
+    a.style.display = "block";
+}
